361b1ff8a317b58fcd44a640bd1e7f8df3f0c7c9
mellonis <mellonis@yandex.ru>
2019-04-06 11:56:41 +0500

    es6 module was described

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import TuringMachine, { Tape, Alphabet, Command, State, haltState, ifOtherSymbol, movements, symbolCommands } from './TuringMachine';
+import binaryNumbers from './std/binaryNumbers';
+
+export { TuringMachine as default, Tape, Alphabet, Command, State, haltState, ifOtherSymbol, movements, symbolCommands, binaryNumbers}
