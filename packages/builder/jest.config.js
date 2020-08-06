f89d1317bf79a20789c69c2897a74600bc2040c5
mellonis <mellonis@yandex.ru>
2020-08-06 23:27:21 +0500

    rollup on next

--- packages/builder/jest.config.js
+++ packages/builder/jest.config.js
@@ -1,6 +1,16 @@
-const moduleName = 'post-machine';
+const packageInfo = require('./package.json');
+
+const [organizationName, packageName] = packageInfo.name.split('/');
 
 module.exports = {
-  name: moduleName,
-  displayName: moduleName,
+  displayName: {
+    name: packageName,
+    color: 'yellow',
+  },
+  moduleNameMapper: {
+    [`^${packageInfo.name}`]: '<rootDir>/src',
+  },
+  transformIgnorePatterns: [
+    `node_modules/(?!${organizationName})`,
+  ],
 };
