241f3ee15f078332d838489aa9fff054f4bd4db0
Ruslan Gilmullin <mellonis@yandex.ru>
2026-03-06 20:21:23 +0300

    Fix repo with amend

--- jest.config.mjs
+++ jest.config.mjs
@@ -3,6 +3,10 @@ export default {
     '.',
     '<rootDir>/packages/*',
   ],
+  moduleNameMapper: {
+    '^@turing-machine-js/machine/src$': '<rootDir>/packages/machine/src',
+    '^@turing-machine-js/builder/src$': '<rootDir>/packages/builder/src',
+  },
   transformIgnorePatterns: [
     'node_modules/(?!@turing-machine-js)',
   ],
